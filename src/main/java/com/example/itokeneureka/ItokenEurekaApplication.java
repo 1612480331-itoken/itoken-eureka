33101068bf3b10959760bc23b90b8366acccdcf5
yls <1329926763@qq.com>
2019-09-16 15:08:14 +0800

    1

---
+++ src/main/java/com/example/itokeneureka/ItokenEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.example.itokeneureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ItokenEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItokenEurekaApplication.class, args);
+    }
+
+}
